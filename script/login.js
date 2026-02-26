b73125411ab651804b90b8f2274429294726b8a6
Pradipta Sarker <techshuvo@gmail.com>
2026-02-26 12:58:46 +0600

    home page dev, js interactions added [basic stuff]

--- script/login.js
+++ script/login.js
@@ -1,1 +1,26 @@
 console.log("login functionlatyu");
+
+document.getElementById("login-btn").addEventListener("click", () => {
+  console.log("login btn clicked");
+
+  //   get contact number
+  const inputNumber = document.getElementById("input-number");
+  const contactNumber = inputNumber.value;
+  console.log(contactNumber);
+
+  /// get Pin value
+  const inputPin = document.getElementById("input-pin");
+  const pinNumber = inputPin.value;
+  console.log(pinNumber);
+
+  //   match the number and pin
+
+  if (contactNumber === "0123456789" && pinNumber === "1234") {
+    alert("Login Successfully");
+    //     window.location.replace("/home.html");
+    window.location.assign("/home.html");
+  } else {
+    alert("login Failed");
+    return;
+  }
+});
