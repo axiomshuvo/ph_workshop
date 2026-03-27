ef5466ae6b1769f215b9e3cb9142f0838fd43ae0
Pradipta Sarker <techshuvo@gmail.com>
2026-03-27 18:28:53 +0600

    practive project -world on the go initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./Components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h4> React practice Project</h4>
+      <Countries />
+    </>
+  );
+}
+
+export default App;
