44d627fb950ae19f48c1e553f0008934be41b217
Pradipta Sarker <techshuvo@gmail.com>
2026-04-03 23:16:50 +0600

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import axios from "axios";
 import "./App.css";
 
-import { Suspense } from "react";
-import Axios from "./components/chart/Axios";
+import Spa from "./components/Spa";
 // import Chart from "./components/chart/Chart";
 
-const marksPromise = axios.get("../public/marksData.json");
+// const marksPromise = axios.get("../public/marksData.json");
 
 export default function App() {
   return (
     <>
       <h1>Line Chart</h1>
       {/* <Chart /> */}
-      <Suspense fallback={<p>Loacing ...</p>}>
+      {/* <Suspense fallback={<p>Loacing ...</p>}>
         <Axios marksPromise={marksPromise}></Axios>
-      </Suspense>
+      </Suspense> */}
+      <Spa />
     </>
   );
 }
---
+++ src/components/Spa.jsx
@@ -0,0 +1,8 @@
+export default function Spa() {
+  return (
+    <div>
+      <h1>Single Page Application</h1>
+      <p>This is a simple single page application.</p>
+    </div>
+  );
+}
